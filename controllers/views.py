018a56e19044f884f2003445ced50095feb2f1dc
William Sousa <willianjohny05@gmail.com>
2026-09-16 16:37:56 -0300

    Add files via upload

--- controllers/views.py
+++ controllers/views.py
@@ -39,3 +39,34 @@ def encontrarPorId():
                 return render_template('detalhes_filme.html', filme=filme)
 
 
+def apagarFilme(id):
+    # Remove o filme com o id fornecido e redireciona para a lista
+    for i, filme in enumerate(filmes):
+        if filme.get('id') == id:
+            del filmes[i]
+            break
+    return redirect('/filmes')
+
+
+def editar(id):
+    # Exibe formulário pré-preenchido (GET) e processa atualização (POST)
+    if request.method == 'GET':
+        for filme in filmes:
+            if filme.get('id') == id:
+                return render_template('editar_filme.html', filme=filme)
+        return redirect('/filmes')
+
+    if request.method == 'POST':
+        titulo = request.form.get('titulo')
+        ano = request.form.get('ano')
+
+        for filme in filmes:
+            if filme.get('id') == id:
+                if titulo:
+                    filme['titulo'] = titulo
+                if ano:
+                    filme['ano'] = ano
+                break
+        return redirect('/filmes')
+
+
--- routes/rotas.py
+++ routes/rotas.py
@@ -1,8 +1,10 @@
-from controllers.views import home, listarTodos, cadastrar, encontrarPorId
+from controllers.views import home, listarTodos, cadastrar, encontrarPorId, editar, apagarFilme
 
 def iniciar_rotas(app):
     app.add_url_rule('/', view_func=home, methods=['GET'])
     app.add_url_rule('/filmes', view_func=listarTodos, methods=['GET'])
     app.add_url_rule('/filmes/cadastrar', view_func=cadastrar, methods=['GET', 'POST'])
     app.add_url_rule('/buscar', view_func=encontrarPorId, methods=['GET'])
+    app.add_url_rule('/filmes/editar/<int:id>', view_func=editar, methods=['GET', 'POST'])
+    app.add_url_rule('/filmes/apagar/<int:id>', view_func=apagarFilme, methods=['POST'])
     
